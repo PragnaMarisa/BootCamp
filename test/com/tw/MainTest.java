b21ab3801b655862caba682023ea4c9f48347cef
Pragna Marisa <158675244+PragnaMarisa@users.noreply.github.com>
2025-05-19 10:39:08 +0530

    First sample working commit

---
+++ test/com/tw/MainTest.java
@@ -0,0 +1,12 @@
+package com.tw;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void onePlusOneIsTwo() {
+        assertEquals(3, 1 + 2);
+    }
+}
